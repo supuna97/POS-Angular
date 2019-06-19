8ec7054159dad6d4480cadc82688eb12ca85f5bd
supunnanayakkara01 <supunnanayakkara01@gmail.com>
2019-06-19 10:38:22 +0530

    create services

--- src/app/view/customer/customer.component.ts
+++ src/app/view/customer/customer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CustomerService} from '../../services/customer.service';
 
 @Component({
   selector: 'app-customer',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+
   }
 
 }
